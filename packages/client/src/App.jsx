c55bd60a8f8317c21f8393f3d2b8b8db517c052a
thanhlam <lam.nguyen@pascal.studio>
2022-01-11 11:45:20 +0700

    add UI interaction

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Card from "./components/card/Card";
 import Navbar from "./components/navbar/Navbar";
+import { posts } from "./data";
 
 function App() {
   const [username, setUsername] = useState("");
@@ -10,14 +11,16 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setUser(username);
-  }
+  };
 
   return (
     <div className="container">
       {user ? (
-        <> 
+        <>
           <Navbar />
-          <Card />
+          {posts.map((post) => (
+            <Card key={post.id} post={post} />
+          ))}
           <span className="username">{user}</span>
         </>
       ) : (
--- packages/client/src/components/card/Card.jsx
+++ packages/client/src/components/card/Card.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+// @ts-nocheck
+import React, { useState } from "react";
+import Heart from "../../img/heart.svg";
+import HeartFilled from "../../img/heartFilled.svg";
+import Comment from "../../img/comment.svg";
+import Share from "../../img/share.svg";
+import Info from "../../img/info.svg";
 import "./Card.css";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+
+  const handleNotification = (e) => {
+    setLiked(true);
+  }
+
+  return (
+    <div className="card">
+      <div className="info">
+        <img src={post?.userImg} alt="user image" className="userImg" />
+        <span>{post?.fullname}</span>
+      </div>
+      <img src={post?.postImg} alt="post image" className="postImg" />
+      <div className="interaction">
+        {liked ? (
+          <img src={HeartFilled} alt="heart" className="cardIcon" />
+        ) : (
+          <img src={Heart} alt="heart" className="cardIcon" onClick={handleNotification} />
+        )}
+        <img src={Comment} alt="comment" className="cardIcon" />
+        <img src={Share} alt="share" className="cardIcon" />
+        <img src={Info} alt="info" className="cardIcon infoIcon" />
+      </div>
+    </div>
+  );
 };
 
 export default Card;
